9bfe17484c3b1799274df357b465656b62808c39
Essi Monneus <essimonneus@gmail.com>
2021-01-23 14:01:04 -0500

    Adding the flask

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ import numpy as np
 from flask import Flask, render_template, jsonify
 import data
 
-app = Flask(__name__, 
-            static_folder='static',
-            template_folder='templates')
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/db_data', methods=['GET'])
-def database_data():
-    # data = data.get_db_data()
-    data = {"this": "is my database data"}
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
     
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+# Save reference to the table
+measurement = Base.classes.measurement
+station= Base.classes.station
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
