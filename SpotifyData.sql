0687c583d8843e0c58a7280b99282beb9e114148
Essi Monneus <essimonneus@gmail.com>
2021-01-23 12:03:56 -0500

    Adding our database

---
+++ SpotifyData.sql
@@ -0,0 +1,2 @@
+SELECT index, valence, year, decade, acousticness, artists, danceability, duration_ms, energy, explicit, instrumentalness, key, liveness, loudness, mode, name, popularity, speechiness, tempo
+	FROM public."SpotifyData";
