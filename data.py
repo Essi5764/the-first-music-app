59348cf75b6160c61c5d82af619586c8e6c16c59
Essi Monneus <essimonneus@gmail.com>
2021-01-25 22:13:53 -0500

    update

--- data.py
+++ data.py
@@ -11,10 +11,20 @@ def get_db_data():
     # Connect to database
     connection_string = "postgres:postgres@localhost:5432/Spotify_project"
     engine = create_engine(f'postgresql://{connection_string}')
+    print(engine.table_names())
     # Query data
-    sql_query = pd.read_sql_query(' Select * FROM SpotifyData', engine)
+    sql_query = pd.read_sql_table('spotifydata', f'postgresql://{connection_string}')
+    # sql_query = engine.execute('SELECT * FROM spotifydata')
+    # d, a = {}, []
+    # for rowproxy in sql_query:
+    #     # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+    #     for column, value in rowproxy.items():
+    #         # build up the dictionary
+    #         d = {**d, **{column: value}}
+    #     a.append(d)
+    #         #print(d)
     # Return data in json format
-    return jsonify(get_db_data)
+    return sql_query.to_dict("records")
 
 # def get_api_data():
 #     # Query to api
